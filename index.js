a40c2b1dc134ca65232f9e114977e6685fb7a07c
Milrren Mattar <milrren.mattar@minutrade.com>
2019-01-23 12:51:35 -0200

    Adding body parser

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const request = require('request');
+const bodyParser = require('body-parser');
 
 const whatever = require('./lib/whatever');
 
 const app = express();
 const router = express.Router();
-
 router.use('/whatever', whatever);
+app.use(bodyParser.json());
 app.use('/', router);
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
